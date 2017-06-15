bce8a57fd3ff556918e796d85ad414a6145a9a6b
Thomas Maier <tmaier@fs.cs.hm.edu>
2017-06-15 23:34:22 +0200

    Add user authentication

--- pms/printing/urls.py
+++ pms/printing/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from django.urls import reverse
 
-from printing.views import IndexView
+from printing.views import DashboardView
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view()),
+    url(r'^$', DashboardView.as_view(), name="home"),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(template_name='printing/login.html'), name='login'),
+    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(template_name='printing/logout.html'), name='logout'),
+    url(r'^dashboard/$', DashboardView.as_view(), name="dashboard"),
 ]
