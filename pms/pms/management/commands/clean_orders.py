7c67c86d9916da5ef31a6b69dc822f17b9a511ef
Ralph Schaumann <absolute512@gmail.com>
2017-09-01 20:12:14 +0200

    Minor ui improvements

--- pms/pms/management/commands/clean_orders.py
+++ pms/pms/management/commands/clean_orders.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from django.core.management import BaseCommand
 
 from printing.models import Order
@@ -7,7 +9,7 @@ class Command(BaseCommand):
     help = 'Delete orders that have not been submitted'
 
     def handle(self, *args, **options):
-        orders = Order.objects.filter(status=0)
+        orders = Order.objects.filter(status=0, create_date__gte=datetime.now()-timedelta(hours=1))
         self.stdout.write('Deleting zombie orders...')
         for order in orders:
             self.stdout.write(f'Deleting {order.order_hash}')
