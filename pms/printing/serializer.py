a8b1281e75b823bc8436e5f4d9449e5a694ce1ea
Anselm Binninger <anselm92@gmail.com>
2017-09-09 14:23:34 +0200

    Allow file upload through rest

--- pms/printing/serializer.py
+++ pms/printing/serializer.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers, viewsets
+from rest_framework.parsers import MultiPartParser, FormParser
 
 from printing.models import Order, Customer
 
@@ -13,8 +14,17 @@ class OrderSerializer(serializers.HyperlinkedModelSerializer):
 
 # ViewSets define the view behavior.
 class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
+    queryset = Order.objects.all().select_subclasses()
     serializer_class = OrderSerializer
+    parser_classes = (MultiPartParser, FormParser,)
+
+    def perform_create(self, serializer):
+        #serializer.save(datafile=self.request.data.get('file'))
+        super(viewsets.ModelViewSet, self).perform_create(serializer)
+
+        # def perform_create(self, serializer):
+        #     serializer.save(owner=self.request.user,
+        #                    datafile=self.request.data.get('datafile'))
 
 
 class CustomerSerializer(serializers.HyperlinkedModelSerializer):
